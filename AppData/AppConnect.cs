7746215ea3a3f5086954dc63eff76641df23378d
10230937 <10230937@D201PC18>
2026-04-17 13:33:57 +0300

    Добавьте файлы проекта.

---
+++ AppData/AppConnect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBowlingClub.AppData.Pages
+{
+    internal class AppConnect
+    {
+        public static Bawling_clubdbEntities model;
+        public static int usersID;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBowlingClub
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppData.Pages.AppConnect.model = new AppData.Bawling_clubdbEntities();
+            AppData.Pages.AppFrame.FrameMain = Frame1;
+            Frame1.Navigate(new AppData.Pages.Authorization());
+        }
+    }
+}
